aa459ef8a1a1e29dcdbd5b3760f83cb2b40ece6b
Natalie <natalie.todd15@gmail.com>
2018-07-29 17:12:19 -0600

    test

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
-/*!
- * express
- * Copyright(c) 2009-2013 TJ Holowaychuk
- * Copyright(c) 2013 Roman Shtylman
- * Copyright(c) 2014-2015 Douglas Christopher Wilson
- * MIT Licensed
- */
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 3000
+const data = require('./api/cohorts');
+const port = parseInt(process.env.PORT || 8080);
+const cohorts = require('./cohorts');
 
 // 'use strict';
 
 // module.exports = require('./lib/express');
+
+app.use(cors())
+
+app.get('/', (req, res, next) => {
+    res.send(JSON.stringify(cohorts))
+})
+
+app.get('/4', req, res, next) => {
+    const person = findMatch(req.params.id, cohorts)
+    if (!person) {
+        res.status = 404
+        res.json({
+            error: {
+                message: 'Error, ID does not exit!'
+            }
+        })
+    }
+    res.json({ cohorts: person })
+}
 
 app.listen(port, () => {
     console.log('listening here ', port)
